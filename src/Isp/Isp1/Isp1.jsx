5861ef699cc542c0ec57d460520ed86de3e21d20
Max <74422818+Ev0lu@users.noreply.github.com>
2024-04-22 00:11:16 +0300

    Add files via upload

--- src/Isp/Isp1/Isp1.jsx
+++ src/Isp/Isp1/Isp1.jsx
@@ -1,16 +1,66 @@
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './Isp1.css';
 import arrowDownSvg from '../../assets/arrow.svg';
-
 function Isp1() {
-  const countries = ['Россия', 'Казахстан', 'Армения', 'Азербайджан', 'Грузия'];
-  const [selectedCountry, setSelectedCountry] = useState('');
-  const [showDropdown, setShowDropdown] = useState(false);
+  const countries = ['Россия', 'Казахстан', 'Армения', 'Азербайджан', 'Грузия' , 'Грузия', 'Грузия', 'Грузия', 'Грузия', 'Грузия', 'Грузия', 'Россия', 'Казахстан', 'Армения', 'Азербайджан', 'Грузия' , 'Грузия', 'Грузия', 'Грузия', 'Грузия'];
   const [city, setCity] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const dropdownRef = useRef(null);
+  const scrollContainerRef = useRef(null);
+  const scrollbarRef = useRef(null);
+  const [scrollbarHeight, setScrollbarHeight] = useState(0);
 
-  const handleCountrySelect = (country) => {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    const calculateScrollbarHeight = () => {
+      const scrollContainerHeight = scrollContainerRef.current.offsetHeight;
+      const contentHeight = scrollContainerRef.current.scrollHeight;
+      const scrollbarHeightPercentage = (scrollContainerHeight / contentHeight) * 100;
+      setScrollbarHeight(scrollbarHeightPercentage);
+    };
+
+    calculateScrollbarHeight();
+
+    const handleResize = () => {
+      calculateScrollbarHeight();
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const selectCountry = (country) => {
     setSelectedCountry(country);
-    setShowDropdown(false);
+    setIsOpen(false);
+  };
+
+  const handleScroll = (e) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.target;
+    const scrollbarHeightPercentage = (clientHeight / scrollHeight) * 100;
+    setScrollbarHeight(scrollbarHeightPercentage);
+    scrollbarRef.current.style.height = `${(scrollbarHeightPercentage)-13}%`;
+    scrollbarRef.current.style.top = `${(scrollTop / scrollHeight) * 100}%`;
   };
 
   return (
@@ -18,34 +68,29 @@ function Isp1() {
         <div className="greetings_wrapper">
 
 
-    <div className="country-selector">
-      <div className="country-dropdown">
-        <div
-          className="selected-country"
-          onClick={() => setShowDropdown(!showDropdown)}
-        >
-          <span>{selectedCountry || 'Страна'}</span>
-          <img
-            src={arrowDownSvg}
-            alt="Arrow Down"
-            className={`arrow-down ${showDropdown && 'rotated'}`}
-          />
+        <div className="dropdown-container" ref={dropdownRef}>
+      <input
+      className='password-field'
+        type="text"
+        value={selectedCountry}
+        placeholder="Select a country"
+        onClick={toggleDropdown}
+        readOnly
+      />
+      <div className={`dropdown-options ${isOpen ? 'open' : ''}`}>
+        <div className="scroll-container" ref={scrollContainerRef} onScroll={handleScroll}>
+          {countries.map((country, index) => (
+            <div key={index} className="dropdown-option" onClick={() => selectCountry(country)}>
+              {country}
+            </div>
+          ))}
         </div>
-        {showDropdown && (
-          <div className="dropdown-list">
-            {countries.map((country, index) => (
-              <div
-                key={index}
-                className={`country ${selectedCountry === country && 'selected'}`}
-                onClick={() => handleCountrySelect(country)}
-              >
-                {country}
-              </div>
-            ))}
-          </div>
-        )}
+        <div className="scrollbar-1" style={{ height: `90%`}} />
+        <div className="scrollbar" ref={scrollbarRef} style={{ height: `${scrollbarHeight}%` }} />
+        
       </div>
-      </div>
+      
+    </div>
       <div className="password-input">
         <input
           type="text"
