81e164df8811afb1a1a34c4ee62c90778fd86595
Max <74422818+Ev0lu@users.noreply.github.com>
2024-04-25 22:03:33 +0300

    Add files via upload

--- src/Isp/Isp.jsx
+++ src/Isp/Isp.jsx
@@ -55,7 +55,7 @@ function Isp(props) {
     return (
         <div className={s.greetings} style={props.colorB==="light" ? {backgroundColor:"white"} : {backgroundColor:"#232323"} }> 
                 <div className={s.greetings_wrapper}>
-        <div className="reg">
+        <div className={s.reg}>
         <Link to='/registration'>
             <img src={props.colorB === 'light' ? blackarr : arrowsvg} className={s.reg_arrow}></img>
         </Link>
