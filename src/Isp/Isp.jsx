d643a94673ea6cd3af4c1e34e6609a0aada4e0cb
Max <74422818+Ev0lu@users.noreply.github.com>
2024-04-29 17:13:11 +0300

    Update Isp.jsx

--- src/Isp/Isp.jsx
+++ src/Isp/Isp.jsx
@@ -75,7 +75,7 @@ function Isp(props) {
         const fetchQuestion = async () => {
             
           
-            setTelegramId(props.tg.initDataUnsafe.user.id)
+            setTelegramId(props.tg.WebAppInitData.user.id)
 
           try {
             const url = `http://localhost/users/check/worker?telegram_id=${telegramId}`;
