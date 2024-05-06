86208377ee67d7a4c9c3fc24ebd40624fc63f458
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-06 17:10:21 +0300

    Update Auth.jsx

--- src/Auth/Auth.jsx
+++ src/Auth/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './Auth.module.css';
 import { Route, Routes, Link, Router } from 'react-router-dom';
 import eyed from '../assets/eye-clos.svg'
