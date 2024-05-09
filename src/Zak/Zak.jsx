88bfdcf3c97bc97fdc46dd24f99d41b4a206c353
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-09 19:09:38 +0300

    Update Zak.jsx

--- src/Zak/Zak.jsx
+++ src/Zak/Zak.jsx
@@ -112,7 +112,7 @@ function Zak(props) {
         
         const fetchData = async () => {
           try {
-            const response = await fetch(`https://assista1.ru/users/check/worker?telegram_id=${telegramId}`);
+            const response = await fetch(`https://assista1.ru/users/check/client?telegram_id=${telegramId}`);
             const data = await response.json();
             const exist = await data.exist;
             setTimeout(setIsexist(JSON.stringify(exist)),1000)
