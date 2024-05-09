172adddab8135eac8a70784c0f296c7cd47be6f3
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-09 12:48:58 +0300

    Update Zak.jsx

--- src/Zak/Zak.jsx
+++ src/Zak/Zak.jsx
@@ -10,6 +10,8 @@ function Zak(props) {
     const [lname, setLname] = useState('');
     const [fname, setFname] = useState('')
     const [fio, setFio] = useState('')
+    const [isexist, setIsexist] = useState(null)
+    const [telegramId, setTelegramId] = useState(props.tg.initDataUnsafe.user.id)
     const [rlink, setRlink] = useState('/zak1_reg')
     const [errorFields, setErrorFields] = useState({
         name: false,
@@ -62,10 +64,94 @@ function Zak(props) {
             name: name === '',
             lname: lname === '',
             gender: gender === ''
+
         };
         setErrorFields(errors);
         return !Object.values(errors).some(Boolean);
     };
+
+
+    /*useEffect(() => {
+        const fetchQuestion = async () => {
+            
+          
+           
+
+          try {
+            const initData = await props.tg.initDataUnsafe;
+            setTelegramId(initData.user.id);
+              
+            const url = `http://localhost/users/check/worker?telegram_id=${telegramId}`;
+            
+            console.log(`Sending request to: ${url}`);
+            const res = await fetch(url);
+            console.log(`Response status: ${res.status}`);
+            if (!res.ok) {
+              throw new Error(`Error: ${res.status}`);
+            }
+            const data = await res.json();
+            const e = data.exist
+            setIsexist(data.exist);
+          } catch (error) {
+            console.error(`Error: ${error.message}`);
+          }
+        };
+        fetchQuestion();
+
+
+        //setTelegramId(props.tg.initDataUnsafe.user.id);
+        fetch(`http://localhost/users/check/worker?telegram_id=${536036487}`)
+        .then(response => response.json())
+        .then(data =>{ setIsexist(JSON.stringify(data.exist))
+        })
+        .catch(console.error)
+      }, []);*/
+
+
+      useEffect(() => {
+        
+        const fetchData = async () => {
+          try {
+            const response = await fetch(`https://assista1.ru/users/check/worker?telegram_id=${telegramId}`);
+            const data = await response.json();
+            const exist = await data.exist;
+            setTimeout(setIsexist(JSON.stringify(exist)),1000)
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        fetchData();
+      }, []);
+      useEffect(() => {
+        setLname(sessionStorage.getItem('lname') !== null ? sessionStorage.getItem('lname') : '')
+        setFname(sessionStorage.getItem('fname') !== null ? sessionStorage.getItem('fname') : '')
+        setName(sessionStorage.getItem('name') !== null ? sessionStorage.getItem('name') : '')
+        setGender(sessionStorage.getItem('gender') !== null ? sessionStorage.getItem('gender') : '')
+
+      }, [])
+   /* const postRequest = async () => {
+        let answersStr = answers.toString()
+        let answersTextStr = answersText.toString()
+        let user = {
+          page_id: window.location.pathname,
+          useragent: window.navigator.userAgent,
+          ip_address: ip,
+          question_id: id,
+          answer_array: answersStr,
+          answer_text: answersTextStr
+        };
+       
+        await fetch('http://62.84.112.244/events/', {
+          method: 'POST',
+          headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(user)
+        });
+        setAnswers([])
+        setAnswersText([])
+      }*/
 
     return (
         <div className={s.greetings} style={props.colorB==="light" ? {backgroundColor:"white"} : {backgroundColor:"#232323"} }> 
@@ -120,11 +206,19 @@ style={props.colorB==='light' ? {backgroundColor:'white', color:'black'} : {back
                 <label htmlFor="female" className={s.genderlabel}>Женский</label>
             </div>
             {gender === '' && (errorFields.gender && <span className={s.error_message}>Выберите ваш пол</span>)}
-        </div>
-        <Link to={gender === '' || name === '' || lname === '' ? '/zak_reg' : '/zak1_reg'}>
+            {isexist === true  && (<span className={s.error_message}>Такой пользователь уже существует</span>) }
+            {isexist === undefined  && (<span className={s.error_message}>Пожалуйста, откройте приложение в телеграме</span>) }        </div>
+            
+        <Link to={gender === '' || name === '' || lname === '' || isexist === true || isexist === null ? '/isp_reg' : '/isp1_reg'}>
             <button className={s.greetings_btn} onClick={() => {
+                sessionStorage.setItem('name', name)
+                sessionStorage.setItem('lname', lname)
+                sessionStorage.setItem('fname', fname)
+                sessionStorage.setItem('gender', gender)
+
                 changeFio()
                 validateFields()
+                console.log(isexist)
             }}>Далее</button>
         </Link>
         </div>
@@ -133,4 +227,3 @@ style={props.colorB==='light' ? {backgroundColor:'white', color:'black'} : {back
   }
   
   export default Zak
-  
