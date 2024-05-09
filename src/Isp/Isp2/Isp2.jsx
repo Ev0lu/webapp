9be196d57fc96176070d7c897920f48716fbf056
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-09 20:53:42 +0300

    Update Isp2.jsx

--- src/Isp/Isp2/Isp2.jsx
+++ src/Isp/Isp2/Isp2.jsx
@@ -103,7 +103,7 @@ const postRequest = async () => {
         <div className={s.greetings} style={props.colorB==="light" ? {backgroundColor:"white"} : {backgroundColor:"#232323"} }>         
         <div className={s.greetings_wrapper}>
         <div className={s.reg}>
-        <Link to='/isp1_reg'>
+        <Link to='/isp3_reg'>
             <img src={props.colorB === 'light' ? blackarr : arrowsvg} className={s.reg_arrow}></img>
         </Link>
             <h1 className={s.greetings_text} style={props.colorB==='light' ? {color:'black'} : {color:'white'} }>Регистрация</h1>
