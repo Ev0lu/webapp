2f98c2cdce94c958a9b2eadef6ac6e0f2ad40b43
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-10 02:03:49 +0300

    Update Create.jsx

--- src/Create/Create.jsx
+++ src/Create/Create.jsx
@@ -315,9 +315,7 @@ const handleInputChange2 = (e) => {
                 <label htmlFor="offline" className={s.genderlabel}>Оффлайн</label>
             </div>
 
-          {place === 'offline'
-            ?
-                    <div className={s.dropdown_container} ref={dropdownRef}>
+          {place === 'offline' && (<div className={s.dropdown_container} ref={dropdownRef}>
                         <input
                           type="text"
                           value={searchQuery}
@@ -366,9 +364,7 @@ const handleInputChange2 = (e) => {
                         </div>
                           {selectedCountry2 === '' && (errorFields.selectedCountry2 && <span className={s.error_message}>Выберите ваш город</span>)}
                   
-                        </div>
-           :
-                         <div>sd</div>
+                        </div>)
            }
       <Link to={(selectedCountry2 === '') || (selectedCountry == '') ? '/isp1_reg' : '/isp3_reg'}>
         <button onClick={() => {
