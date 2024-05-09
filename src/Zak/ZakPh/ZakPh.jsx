c0a8ec5971e02393e063090592fee0d9b417b79f
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-09 18:23:06 +0300

    Update ZakPh.jsx

--- src/Zak/ZakPh/ZakPh.jsx
+++ src/Zak/ZakPh/ZakPh.jsx
@@ -1,104 +1,119 @@
-import { Route, Routes, Link, Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import s from './ZakPh.module.css';
 import plusSvg from '../../assets/plus2.svg';
 import pencilSvg from '../../assets/pencil.svg';
-import { useEffect, useState } from 'react';
-function ZakPh(props) {
-  const [isRegistered, setIsRegistered] = useState(false);
-  const [isRegistered2, setIsRegistered2] = useState(false);
+import { useState } from 'react';
 
-    const [avatar, setAvatar] = useState(null);
-    const [showEditIcon, setShowEditIcon] = useState(false);
-    const [errorFields, setErrorFields] = useState({
-      avatar: false
+function ZakPh(props) {
+  const [avatar, setAvatar] = useState(null);
+  const [size, setSize] = useState(false);
+  const [filepic, setFilepic] = useState(null);
+  const [errorFields, setErrorFields] = useState({
+    avatar: false,
+    size: false
   });
+
+const handleAvatarChange = (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const img = new Image();
+      img.onload = () => {
+        if (img.width > 200 || img.height > 200) {
+          setErrorFields({ size: true });
+          setAvatar(null);
+        } else {
+          setErrorFields({ size: false });
+          setAvatar(reader.result);
+          setFilepic(file)
+        }
+      };
+      img.src = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+};
+
   const validateFields = () => {
     const errors = {
-      avatar: avatar === null
+      avatar: avatar === null,
+      size: size === false
     };
     setErrorFields(errors);
     return !Object.values(errors).some(Boolean);
+  };
+
+const uploadPhoto = async () => {
+  const formData = new FormData();
+  formData.append('photo', filepic);
+
+  try {
+    const response = await fetch('https://assista1.ru/users/uploadPhoto', {
+      method: 'PATCH',
+      body: formData
+    });
+    if (response.ok) {
+      const responseData = await response.json();
+      console.log(responseData);
+      // Handle response data if needed
+    } else {
+      console.error('Failed to upload photo');
+    }
+  } catch (error) {
+    console.error('Error uploading photo:', error);
+  }
 };
 
-    const handleAvatarChange = (event) => {
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = () => {
-          setAvatar(reader.result);
-          setShowEditIcon(true);
-        };
-        reader.readAsDataURL(file);
-      }
-    };
-  
-    const handleEditAvatar = () => {
-      const input = document.createElement('input');
-      input.type = 'file';
-      input.accept = 'image/*';
-      input.onchange = (event) => {
-        handleAvatarChange(event);
-      };
-      input.click();
-    };
-    return (
-      <div className={s.greetings} style={props.colorB==="light" ? {backgroundColor:"white"} : {backgroundColor:"#232323"} }> 
-                <div className={s.greetings_wrapper}>
-      <h1 className={s.greetings_text} style={props.colorB==='light' ? {color:'black'} : {color:'white'} }>Регистрация</h1>
-      <div className={s.avatar_container}>
-      <label htmlFor={s.avatar_input} className={s.avatar}>
-        {avatar ? (
-          <>
-            <img
-              src={avatar}
-              alt="Avatar"
-              className={s.avatar_img}
 
-            />
-            {showEditIcon && (
-              <img
-                src={pencilSvg}
-                alt="Edit Avatar"
-                className={s.edit_icon}
-                onClick={handleEditAvatar}
-              />
+  return (
+    <div className={s.greetings} style={props.colorB === "light" ? {backgroundColor: "white"} : {backgroundColor: "#232323"} }>
+      <div className={s.greetings_wrapper}>
+        <h1 className={s.greetings_text} style={props.colorB === 'light' ? {color: 'black'} : {color: 'white'} }>Регистрация</h1>
+        <div className={s.avatar_container}>
+          <label htmlFor="avatar_input" className={s.avatar}>
+            {avatar ? (
+              <>
+                <img
+                  src={avatar}
+                  alt="Avatar"
+                  className={s.avatar_img}
+                />
+                <img
+                  src={pencilSvg}
+                  alt="Edit Avatar"
+                  className={s.edit_icon}
+                />
+              </>
+            ) : (
+              <div className={s.avatar_placeholder}>
+                <img src={plusSvg} alt="Add Avatar" className={s.plus_svg} />
+              </div>
             )}
-          </>
-        ) : (
-          <div className={s.avatar_placeholder} onClick={handleEditAvatar}>
-            <img src={plusSvg} alt="Add Avatar" className={s.plus_svg} />
+          </label>
+          <div className={s.avatar_flex}>
+            <input
+              id="avatar_input"
+              type="file"
+              accept="image/*"
+              onChange={handleAvatarChange}
+              className={s.avatar_input}
+            />
+            {avatar === null && (errorFields.avatar && <span className={s.error_message}>Пожалуйста, приложите изображение</span>)}
+            {size === true && (errorFields.size && <span className={s.error_message}>Изображение не должно быть размером больше чем 200x200</span>)}
           </div>
-        )}
-      </label>
-      <div className={s.avatar_flex}>
-        <input
-          id={s.avatar_input}
-          type="file"
-          accept="image/*"
-          onChange={handleAvatarChange}
-          className={s.avatar_input}
-        />
-        { avatar === null && (errorFields.avatar && <span className={s.error_message}>Пожалуйста, приложите изображение</span>)}
+        </div>
+        <Link to={avatar !== null ? '/create' : '/zak_reg_photo'}>
+          <button onClick={() => {
+          validateFields()
+          uploadPhoto()}} className={`${s.greetings_btn} ${props.colorB === 'light' ? s.lightMode : s.darkMode}`}>Далее</button>
+        </Link>
+        <Link to={'/create'}>
+          <button className={`${s.greetings_btn} ${props.colorB === 'light' ? s.lightMode : s.darkMode}`}>пропустить</button>
+        </Link>
       </div>
     </div>
-          <Link to={avatar !== null ? '/create' : '/zak_reg_photo'}>
-          <button onClick={() => {
+  );
+}
 
-                    validateFields()
-                }} className={`${s.greetings_btn} ${isRegistered ? s.lightMode1 : (props.colorB === 'light' ? s.lightMode : s.darkMode)}`}>Далее</button>
-          </Link>
-          <Link to={'/'}>
-
-          <button onClick={() => {
-                    setIsRegistered2(true)
-                    setIsRegistered(false)
-                    
-                }} className={`${s.greetings_btn} ${isRegistered2 ? s.lightMode1 : (props.colorB === 'light' ? s.lightMode : s.darkMode)}`}>пропустить</button>
-          </Link>
-
-        </div>
-      </div>
-    )
-  }
-  
-  export default ZakPh
+export default ZakPh;
