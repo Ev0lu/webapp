5d157965b8251dd8c27d32c2cf20e634bdfa7dcc
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-09 15:05:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
           <Route path="/registration" element={<Reg colorB={colorB} />} />
           <Route path="/authorization" element={<Auth colorB={colorB} />} />
           <Route path="/isp_reg" element={<Isp tg={tg} colorB={colorB}/>} />
-          <Route path="/zak_reg" element={<Zak colorB={colorB} />} />
+          <Route path="/zak_reg" element={<Zak tg={tg} colorB={colorB} />} />
           <Route path="/zak1_reg" element={<Zak1 colorB={colorB} />} />
           <Route path="/zak2_reg" element={<Zak2 colorB={colorB} />} />
           <Route path="/zak_reg_photo" element={<ZakPh colorB={colorB} />} />
