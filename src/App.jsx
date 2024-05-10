766cd10b96cc3ec8109ee5e929a176a9490b2f72
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-10 12:42:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,6 @@ function App() {
           <Route path="/isp_reg_photo" element={<IspPh colorB={colorB} />} />
           <Route path="/isp_con" element={<IspCon tg={tg} colorB={colorB} />} />
           <Route path="/zak_con" element={<ZakCon tg={tg} colorB={colorB} />} />
-          <Route path="/payment" element={<Payment tg={tg} colorB={colorB} />} />
 
     
           
