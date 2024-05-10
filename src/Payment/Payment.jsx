b2f44d1b8ee60b36ef7dade79226ee2a4d3b79cf
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-10 12:08:02 +0300

    Update Payment.jsx

--- src/Payment/Payment.jsx
+++ src/Payment/Payment.jsx
@@ -1,28 +1,18 @@
 import { useState } from "react";
-import s from "./Greetings.module.css"
+import s from "./Payment.module.css"
 import { Route, Routes, Link, Router } from 'react-router-dom';
-function Greetings(props) {
+function Payment(props) {
     const [isRegistered, setIsRegistered] = useState(false);
     const [isRegistered2, setIsRegistered2] = useState(false);
     return (
         <div className={s.greetings} style={props.colorB==="light" ? {backgroundColor:"white"} : {backgroundColor:"#232323"}}>   
         <div className={s.greetings_wrapper}>
-        <h1 className={s.greetings_text} style={props.colorB==='light' ? {color:'black'} : {color:'white'} }>Добро пожаловать</h1>
-            <Link to="/registration">
-                <button onClick={() => {setIsRegistered(true)}} className={`${s.greetings_btn} ${isRegistered ? s.lightMode1 : (props.colorB === 'light' ? s.lightMode : s.darkMode)}`}>Зарегистироваться</button>
-            </Link> 
-            <Link to="/authorization">
-                <button onClick={() => {
-                    setIsRegistered2(true)
-                    setIsRegistered(false)
-                
-                }} className={`${s.greetings_btn} ${isRegistered2 ? s.lightMode1 : (props.colorB === 'light' ? s.lightMode : s.darkMode)}`}>У меня уже есть аккаунт</button>
-            </Link>
+        <h1 className={s.greetings_text} style={props.colorB==='light' ? {color:'black'} : {color:'white'} }>Оплата прошла успешно</h1>
             </div>
         </div>
 
     )
   }
   
-  export default Greetings
+  export default Payment
   
