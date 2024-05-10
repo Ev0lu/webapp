70209a8f46d87f3f69b1d65ae187f29fcc5e51af
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-11 01:56:48 +0300

    Update Zak1.jsx

--- src/Zak/Zak1/Zak1.jsx
+++ src/Zak/Zak1/Zak1.jsx
@@ -136,7 +136,7 @@ function Zak1(props) {
         <button onClick={() => {
           validateFields()
           if (selectedDate !== null || err !== false){
-              sessionStorage.setItem('birth_date', `${selectedYear}-${selectedMonth+1}-${selectedDate}`)
+              sessionStorage.setItem('birth_date', `${selectedYear}-${selectedMonth+1}-${selectedDate.getDate()}`)
           }
 
       }} className={`${s.greetings_btn} ${props.colorB === 'light' ? s.lightMode : s.darkMode}`}>Далее</button>
