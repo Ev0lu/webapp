b30027532f87faeadd74f857a4601da57d1e9c70
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-12 11:00:59 +0300

    Update SuccessReg.jsx

--- src/SuccessReg/SuccessReg.jsx
+++ src/SuccessReg/SuccessReg.jsx
@@ -7,7 +7,7 @@ function Payment(props) {
     return (
         <div className={s.greetings} style={props.colorB==="light" ? {backgroundColor:"white"} : {backgroundColor:"#232323"}}>   
         <div className={s.greetings_wrapper}>
-        <h1 className={s.greetings_text} style={props.colorB==='light' ? {color:'black'} : {color:'white'} }>Оплата прошла успешно</h1>
+        <h1 className={s.greetings_text} style={props.colorB==='light' ? {color:'black'} : {color:'white'} }>Вы успешно зарегистрированы</h1>
             </div>
         </div>
 
