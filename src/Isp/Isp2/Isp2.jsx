a13bf1485c7d3e0c517814130c591b55ae653624
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-12 13:27:56 +0300

    Update Isp2.jsx

--- src/Isp/Isp2/Isp2.jsx
+++ src/Isp/Isp2/Isp2.jsx
@@ -39,7 +39,7 @@ function Isp2(props) {
 
 
     const handleChange = (event) => {
-        setLogin(event.target.value.replace(/[^A-Za-z]/g, ''));
+        setLogin(event.target.value.replace(/[^A-Za-z0-9]/g, ''));
     };
     const handleChange2 = (event) => {
         const isValidPhone = /^\+/.test(event.target.value)
