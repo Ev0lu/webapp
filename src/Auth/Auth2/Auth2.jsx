46cf9cbb7046fc6ff95b47d6dc87341de5fd325d
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-13 11:34:31 +0300

    Update Auth2.jsx

--- src/Auth/Auth2/Auth2.jsx
+++ src/Auth/Auth2/Auth2.jsx
@@ -88,7 +88,7 @@ const postRequest = async () => {
         <div className={s.greetings} style={props.colorB==="light" ? {backgroundColor:"white"} : {backgroundColor:"#232323"} }>         
         <div className={s.greetings_wrapper}>
         <div className={s.reg}>
-        <Link to='/isp1_reg'>
+        <Link to='/authorization'>
             <img src={props.colorB === 'light' ? blackarr : arrowsvg} className={s.reg_arrow}></img>
         </Link>
             <h1 className={s.greetings_text} style={props.colorB==='light' ? {color:'black'} : {color:'white'} }>Регистрация</h1>
