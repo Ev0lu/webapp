b9af60a6708755f2073120c837432bbd455faf54
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-13 23:10:36 +0300

    Update ZakCon.jsx

--- src/Zak/ZakCon/ZakCon.jsx
+++ src/Zak/ZakCon/ZakCon.jsx
@@ -147,12 +147,16 @@ const postRequest = async () => {
       sessionStorage.setItem('access_token', data.access_token)
       sessionStorage.setItem('profile_id', data.profile_id)
       sessionStorage.setItem('refresh_token', data.refresh_token)
-
+        const data = await response.json();
+      console.log(data)
     } else {
       const data = await response.json();
+      console.log(data)
     }
 
   } catch (error) {
+  
+      console.log(error)
   }
 }
   return (
