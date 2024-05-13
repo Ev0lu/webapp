0470500cfa2752b7145554c51201aa154db5f348
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-13 22:35:22 +0300

    Update Isp1.jsx

--- src/Isp/Isp1/Isp1.jsx
+++ src/Isp/Isp1/Isp1.jsx
@@ -1,3 +1,10 @@
+import React, { useRef, useState, useEffect } from 'react';
+import s from './Isp1.module.css';
+import arrowsvg from '../../assets/arrow.svg';
+import arrowsvg2 from '../../assets/angle-dark.svg';
+import blackarr from '../../assets/black.svg';
+import { Link } from 'react-router-dom';
+
 function Isp1(props) {
   const [countries, setCountries] = useState([]);
   const [loading, setLoading] = useState(false);
