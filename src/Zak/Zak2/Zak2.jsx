e960d312be182ce1ba3a84adb16ffd20cc792e13
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-13 16:00:50 +0300

    Update Zak2.jsx

--- src/Zak/Zak2/Zak2.jsx
+++ src/Zak/Zak2/Zak2.jsx
@@ -24,7 +24,7 @@ function Zak2(props) {
     });
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const [exist, setExist] = useState(`${sessionStorage.getItem('exist')}`);
+  const [exist, setExist] = useState(sessionStorage.getItem('exist'));
   const [accessToken, setAccessToken] = useState(sessionStorage.getItem('accessToken'));
     
     const validateFields = () => {
@@ -237,13 +237,13 @@ const checkUniqueF = async () => {
              </div>
       
 
-        <Link to={login !== '' && tele !== '' && mail !== '' && check !== '' && checkPh !== ''&& {exist === 'false' && checkUnique !== '' } ? '/zak_con' : '/zak2_reg'}>
+        <Link to={login !== '' && tele !== '' && mail !== '' && check !== '' && checkPh !== ''&& {exist === false && checkUnique !== '' } ? '/zak_con' : '/zak2_reg'}>
             <button className={`${s.greetings_btn}`} onClick={() => {
                 sessionStorage.setItem('login', login)
                 sessionStorage.setItem('tele', tele)
                 sessionStorage.setItem('mail', mail)
 
-                if (login !== '' && tele !== '' && mail !== '' && check !== '' && checkPh !== '' && {exist === 'false' && checkUnique !== '' }) {
+                if (login !== '' && tele !== '' && mail !== '' && check !== '' && checkPh !== '' && {exist === false && checkUnique !== '' }) {
                     postRequest()
                 }
                 validateFields()
