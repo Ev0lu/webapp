75ea02c96b46eba7dd7503ea024f7ea116e1b5ed
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-13 13:11:17 +0300

    Update IspCon.jsx

--- src/Isp/IspCon/IspCon.jsx
+++ src/Isp/IspCon/IspCon.jsx
@@ -161,7 +161,7 @@ const postRequest = async () => {
         </div>
         {error && <div className={s.error_message}>Неправильный код</div>}
         {tries > 3  && <div className={s.error_message}>Вы исчерпали количество попыток, начните регистрацию заново</div>}
-    <Link to={code1 == '' || code2 == '' || code3 == '' || code4 == '' || tries > 3 || isVerified === false ? '/isp_con' : '/isp3_reg'}>
+    <Link to={code1 == '' || code2 == '' || code3 == '' || code4 == '' || tries > 3 || isVerified === false ? '/isp_con' : '/isp_reg_pass'}>
         <button className={`${s.greetings_btn} ${props.colorB === 'light' ? s.lightMode : s.darkMode}`} onClick={() => {
 
       }}>
