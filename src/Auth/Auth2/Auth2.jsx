5f65d9324acbbff1f5e2dd56087d0d8865abc9a9
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-13 11:03:51 +0300

    Update Auth2.jsx

--- src/Auth/Auth2/Auth2.jsx
+++ src/Auth/Auth2/Auth2.jsx
@@ -14,42 +14,26 @@ function Isp2(props) {
     const [checkPh,setCheckPh] = useState('')
     const [rlink, setRlink] = useState('/zak1_reg')
     const [errorFields, setErrorFields] = useState({
-        login: false,
-        tele: false,
+
         mail: false,
-        pass: false,
-        pass2: false,
+
         check: false,
-        checkPh: false
     });
 
     const validateFields = () => {
         const errors = {
-            login: login === '',
-            tele: tele === '',
+
             mail: mail === '',
-            pass: pass === '',
-            pass2: pass2 === '',
+
             check: check === '',
-            checkPh: checkPh === ''
         };
         setErrorFields(errors);
         return !Object.values(errors).some(Boolean);
     };
 
 
-    const handleChange = (event) => {
-        setLogin(event.target.value.replace(/[^A-Za-z0-9]/g, ''));
-    };
-    const handleChange2 = (event) => {
-        const isValidPhone = /^\+/.test(event.target.value)
-        if (isValidPhone === true) {
-            setCheckPh('exist')
-        } else{
-            setCheckPh('')
-        }
-        setTele(event.target.value);
-    };
+
+
     const handleChange3 = (event) => {
         setMail(event.target.value);
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -59,12 +43,6 @@ function Isp2(props) {
         } else{
             setCheck('')
         }
-    };
-    const handleChange4 = (event) => {
-        setPass(event.target.value);
-    };
-    const handleChange5 = (event) => {
-        setPass2(event.target.value);
     };
 
 
@@ -79,11 +57,9 @@ function Isp2(props) {
         }
     }
     useEffect(() => {
-        setLogin(sessionStorage.getItem('login') !== null ? sessionStorage.getItem('login') : '')
-        setTele(sessionStorage.getItem('tele') !== null ? sessionStorage.getItem('tele') : '')
+
         setMail(sessionStorage.getItem('mail') !== null ? sessionStorage.getItem('mail') : '')
-        setPass(sessionStorage.getItem('pass') !== null ? sessionStorage.getItem('pass') : '')
-        setPass2(sessionStorage.getItem('pass') !== null ? sessionStorage.getItem('pass') : '')
+
 
       }, [])
 
@@ -94,7 +70,7 @@ const postRequest = async () => {
   };
 
   try {
-    const response = await fetch('https://assista1.ru/api/v1/auth/code/send', {
+    const response = await fetch('https://assista1.ru/api/v1/users/forgotPassword/code', {
       method: 'POST',
       headers: {
         'accept': 'application/json',
@@ -117,35 +93,8 @@ const postRequest = async () => {
         </Link>
             <h1 className={s.greetings_text} style={props.colorB==='light' ? {color:'black'} : {color:'white'} }>Регистрация</h1>
         </div>
-        <div className={s.password_input}>
-            <input
-                type={'text'}
-                placeholder="Логин"
-                className={`${s.password_field} ${errorFields.login && s.error}`}
-                value={login}
-                onChange={handleChange}
-                style={props.colorB==='light' ? {backgroundColor:'white', color:'black'} : {backgroundColor:'#232323', color:'#C7C7C7'} }
-
-            />
-            {login === '' && (errorFields.login && <span className={s.error_message}>Пожалуйста, введите логин</span>)}
-            {/^[A-Za-z0-9]+$/.test(login) === false && <span className={s.error_message}>Логин поддерживает только латинский алфавит</span>}
-
-        </div>
-        <div className={s.password_input}>
-            <input
-                type={'text'}
-                placeholder="Номер телефона"
-                className={`${s.password_field} ${(errorFields.tele || errorFields.checkPh) && s.error}`}
-                value={tele}
-                onChange={handleChange2}
-                style={props.colorB==='light' ? {backgroundColor:'white', color:'black'} : {backgroundColor:'#232323', color:'#C7C7C7'} }
-
-            />
-        {tele === '' && (errorFields.tele && <span className={s.error_message}>Пожалуйста, введите телефон</span>)}
-        {errorFields.checkPh && <span className={s.error_message}>Номер должен начинаться с кода страны(+...)</span>}
 
 
-        </div>
         <div className={s.password_input}>
             <input
                 type={'text'}
@@ -157,49 +106,17 @@ const postRequest = async () => {
 
             />
             {mail === '' && (errorFields.mail && <span className={s.error_message}>Пожалуйста, введите почту</span>)}
+            {errorFields.check && <span className={s.error_message}>Почта не соответствует формату</span>}
 
              </div>
-        <div className={s.password_input}>
-            <input
-                type={'text'}
-                placeholder="Пароль"
-                className={`${s.password_field} ${errorFields.pass && s.error}`}
-                value={pass}
-                onChange={handleChange4}
-                style={props.colorB==='light' ? {backgroundColor:'white', color:'black'} : {backgroundColor:'#232323', color:'#C7C7C7'} }
-
-            />
-            {pass === '' && (errorFields.pass && <span className={s.error_message}>Пожалуйста, введите пароль</span>)}
-
-             </div>
-        <div className={s.password_input}>
-             <input
-                type={'text'}
-                placeholder="Подтверждение пароля"
-                className={`${s.password_field} ${errorFields.pass2 && s.error}`}
-                value={pass2}
-                onChange={handleChange5}
-                style={props.colorB==='light' ? {backgroundColor:'white', color:'black'} : {backgroundColor:'#232323', color:'#C7C7C7'} }
-
-            />
-        { pass === '' && (errorFields.pass2 && <span className={s.error_message}>Пожалуйста, подтвердите пароль</span>)}
-        {pass!=pass2 && <span className={s.error_message}>Пароли должны совпадать</span>}
-        {errorFields.check && <span className={s.error_message}>Почта не соответствует формату</span>}
-        { pass.length<10 && <span className={s.error_message}>Размер пароля должен составлять от 10 до 25 символов</span>}
-        {pass.length > 24 && <span className={s.error_message}>Размер пароля должен составлять от 10 до 25 символов</span>}
 
 
-
-
-        </div>
-
-        <Link to={pass === pass2 && login !== '' && tele !== '' && mail !== '' && check !== '' && checkPh !== '' && (pass.length>9) && (pass.length < 25) ? '/isp_con' : '/isp2_reg'}>
+        <Link to={mail !== '' && check !== '' ? '/auth_con' : '/auth2'}>
             <button className={`${s.greetings_btn}`} onClick={() => {
-                sessionStorage.setItem('login', login)
-                sessionStorage.setItem('tele', tele)
+
                 sessionStorage.setItem('mail', mail)
-                sessionStorage.setItem('pass', pass)
-                if (pass === pass2 && login !== '' && tele !== '' && mail !== '' && check !== '' && checkPh !== '') {
+
+                if (mail !== '' && check !== '') {
                     postRequest()
                 }
                 validateFields()
