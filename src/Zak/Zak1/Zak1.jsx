12711db7e40ef97b09d60fa8c9dc53ec809614c7
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-14 15:10:15 +0300

    Update Zak1.jsx

--- src/Zak/Zak1/Zak1.jsx
+++ src/Zak/Zak1/Zak1.jsx
@@ -98,13 +98,13 @@ function Zak1(props) {
         readOnly
       />
 
-
+      {selectedDate === null && (errorFields.selectedDate && <span className={s.error_message}>Пожалуйста, введите дату</span>)}
+      {errorFields.err && <span className={s.error_message}>Вы должны быть старше 18 лет</span>}
       </div>
       <div className={`${s.icon} `} onClick={toggleCalendar}>
         {showCalendar ? <img src={props.colorB === 'light' ? lightplus : plus}></img>: <img src={props.colorB === 'dark' ? minus : lightminus}></img>}
       </div>
-      {selectedDate === null && (errorFields.selectedDate && <span className={s.error_message}>Пожалуйста, введите дату</span>)}
-      {errorFields.err && <span className={s.error_message}>Вы должны быть старше 18 лет</span>}
+
       {showCalendar && (
         <div className={`${s.calendar} ${props.colorB === 'light' ? s.light : s.dark}`}>
           <div className={s.nav}>
