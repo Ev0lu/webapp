1b63261a07ccc8eca4157dda33d781fa9f28f1a0
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-15 00:35:33 +0300

    Update Zak2.jsx

--- src/Zak/Zak2/Zak2.jsx
+++ src/Zak/Zak2/Zak2.jsx
@@ -143,7 +143,6 @@ const checkUniqueF = async () => {
         
          const responseData = await response.json();
       // Handle response data if needed
-        console.log(responseData)
       if (responseData.detail[0]?.msg ? responseData.detail[0].msg.includes("login") : responseData.detail.includes("login")){
             setLoginerr('true')
         } else{
