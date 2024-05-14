f78d47916d55e9fa6dcaf8039e8d379ccbaaecb0
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-14 05:53:42 +0300

    Update Zak2.jsx

--- src/Zak/Zak2/Zak2.jsx
+++ src/Zak/Zak2/Zak2.jsx
@@ -270,7 +270,7 @@ const checkUniqueF = async () => {
         {teleCon === '' && (errorFields.tele && <span className={s.error_message}>Пожалуйста, введите телефон</span>)}
         {tele.split('').length < 7 && (errorFields.tele && <span className={s.error_message}>Пожалуйста, введите правильный телефон</span>)}
         {errorFields.checkPh && <span className={s.error_message}>Номер должен начинаться с кода страны(+...)</span>}
-        {teleerr === 'true' && <span className={s.error_message}>Почта уже существует</span> }
+        {teleerr === 'true' && <span className={s.error_message}>Телефон уже существует</span> }
 
         </div>
         <div className={s.password_input}>
