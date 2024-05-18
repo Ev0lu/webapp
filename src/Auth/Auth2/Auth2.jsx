0fd60be625800dfaef9bd53ed0eebeb574875a3a
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-18 10:40:22 +0300

    Add files via upload

---
+++ src/Auth/Auth2/Auth2.jsx
@@ -0,0 +1,138 @@
+import { useEffect, useState } from "react";
+import s from "./Auth2.module.css"
+import { Link } from "react-router-dom";
+import arrowsvg from '../../assets/arrow.svg'
+import blackarr from '../../assets/black.svg'
+
+function Auth2(props) {
+    const [login, setLogin] = useState('');
+    const [tele, setTele] = useState('');
+    const [mail, setMail] = useState('');
+    const [pass, setPass] = useState('')
+    const [pass2, setPass2] = useState('')
+    const [check,setCheck] = useState('')
+    const [checkPh,setCheckPh] = useState('')
+    const [rlink, setRlink] = useState('/zak1_reg')
+    const [checkEx, setCheckEx] = useState('exist')
+    const [errorFields, setErrorFields] = useState({
+
+        mail: false,
+
+        check: false,
+        checkEx: false
+    });
+
+    const validateFields = () => {
+        const errors = {
+
+            mail: mail === '',
+
+            check: check === '',
+            checkEx: checkEx === ''
+        };
+        setErrorFields(errors);
+        return !Object.values(errors).some(Boolean);
+    };
+
+
+
+
+    const handleChange3 = (event) => {
+        setMail(event.target.value);
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const isValidEmail = emailRegex.test(event.target.value);
+        if (isValidEmail === true) {
+            setCheck('exist')
+        } else{
+            setCheck('')
+        }
+    };
+
+
+
+
+    const checking = () => {
+        if ((pass !== pass2) || (login === '') || (tele === '') || (mail === '')) {
+            console.log('error')
+            setRlink('/zak_reg')
+        } else {
+            setRlink('/zak1_reg')
+        }
+    }
+    useEffect(() => {
+
+        setMail(sessionStorage.getItem('mail') !== null ? sessionStorage.getItem('mail') : '')
+
+
+      }, [])
+
+        
+const postRequest = async () => {  
+  let user = {
+    email: mail,
+  };
+
+  try {
+    const response = await fetch('https://assista1.ru/api/v1/users/forgotPassword/code', {
+      method: 'POST',
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+    if (response.status === 400){
+        setCheckEx('')
+    }
+  } catch (error) {
+    setCheck('exist')
+  }
+}
+    
+    return (
+        <div className={s.greetings} style={props.colorB==="light" ? {backgroundColor:"white"} : {backgroundColor:"#232323"} }>         
+        <div className={s.greetings_wrapper}>
+        <div className={s.reg}>
+        <Link to='/authorization'>
+            <img src={props.colorB === 'light' ? blackarr : arrowsvg} className={s.reg_arrow}></img>
+        </Link>
+            <h1 className={s.greetings_text} style={props.colorB==='light' ? {color:'black'} : {color:'white'} }>Введите почту</h1>
+        </div>
+
+
+        <div className={s.password_input}>
+            <input
+                type={'text'}
+                placeholder="Почта"
+                className={`${s.password_field} ${errorFields.mail && s.error}`}
+                value={mail}
+                onChange={handleChange3}
+                style={props.colorB==='light' ? {backgroundColor:'white', color:'black'} : {backgroundColor:'#232323', color:'#C7C7C7'} }
+
+            />
+            {mail === '' && (errorFields.mail && <span className={s.error_message}>Пожалуйста, введите почту</span>)}
+            {errorFields.check && <span className={s.error_message}>Почта не соответствует формату</span>}
+            {errorFields.checkEx && <span className={s.error_message}>Почта не соответствует формату</span>}
+
+
+             </div>
+
+
+        <Link to={mail !== '' && check !== '' && checkEx !== '' ? '/authorization_verify' : '/authorization_mail'}>
+            <button className={`${s.greetings_btn}`} onClick={() => {
+
+                sessionStorage.setItem('mail', mail)
+
+                if (mail !== '' && check !== '') {
+                    postRequest()
+                }
+                validateFields()
+            }}>Далее</button>
+        </Link>
+        </div>
+      </div>
+    )
+  }
+  
+export default Auth2;
+  
