c51729974306bdb61bc470b9579621fb3692db1f
Max <74422818+Ev0lu@users.noreply.github.com>
2024-05-26 17:34:32 +0300

    Add files via upload

--- src/Zak/Zak1/Zak1.jsx
+++ src/Zak/Zak1/Zak1.jsx
@@ -75,7 +75,7 @@ function Zak1(props) {
     
       const handleDateClick = (day) => {
         setSelectedDate(new Date(selectedYear, selectedMonth, day));
-        setSelectedDateStr(`${day < 10 ? '0' : ''}${day}.${selectedMonth - 1 < 10 ? '0' : ''}${selectedMonth + 1}.${selectedYear}`);
+        setSelectedDateStr(`${day < 10 ? '0' : ''}${day}.${selectedMonth - 1 < 8 ? '0' : ''}${selectedMonth + 1}.${selectedYear}`);
 
         setShowCalendar(!showCalendar);
 
