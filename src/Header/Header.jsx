c7cf1d96d45d0ae12a8639ca1317ce37be6a59b6
Max <74422818+Ev0lu@users.noreply.github.com>
2024-06-06 15:37:20 +0300

    Add files via upload

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,11 @@
-import logo from '../assets/logo-1.svg'
+import logo from '../assets/newLogo.png'
 import '../Header/Header.css'
 function Header() {
 
   return (
    <div className={'header'}>
-    <div className="image">
-        <img src={logo}></img>
+    <div className="imageLogo">
+        <img className='image' src={logo}></img>
     </div>
    </div>
   )
