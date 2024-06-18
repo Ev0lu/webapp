de5e6eb714c77c9b183870564530777dd809dd3e
Max <74422818+Ev0lu@users.noreply.github.com>
2024-06-18 11:53:02 +0300

    Update Isp.jsx

--- src/Isp/Isp.jsx
+++ src/Isp/Isp.jsx
@@ -54,18 +54,18 @@ const refreshTok = async () => {
 }
 
 const handleChange = (event) => {
-  const sanitizedValue = event.target.value.replace(/[<>%$&!*^`/"',.|#@()\[\]{}\s0-9]/g, '');
+  const sanitizedValue = event.target.value.replace(/[<>%$&!*^`/"',.|#@()\[\]{}\s0-9:;?]/g, '');
   
     setName(sanitizedValue);
   
 };
 const handleChange2 = (event) => {
-  const sanitizedValue = event.target.value.replace(/[<>%$&!*^`/"',.|#@()\[\]{}\s0-9]/g, '');
+  const sanitizedValue = event.target.value.replace(/[<>%$&!*^`/"',.|#@()\[\]{}\s0-9:;?]/g, '');
   
     setLname(sanitizedValue);
 };
 const handleChange3 = (event) => {
-  const sanitizedValue = event.target.value.replace(/[<>%$&!*^`/"',.|#@()\[\]{}\s0-9]/g, '');
+  const sanitizedValue = event.target.value.replace(/[<>%$&!*^`/"',.|#@()\[\]{}\s0-9:;?]/g, '');
   
     setFname(sanitizedValue);
 };
